3bfe067b31310188b567e3e106870c29885c85d2
MAPDOM\bdavis <bdavis@mapsysinc.com>
2019-03-21 22:04:06 -0400

    add template for page to center infographic

---
+++ BriefHistory.php
@@ -0,0 +1,25 @@
+<?php /* Template Name: BriefHistory */ ?>
+
+<?php
+/**
+ * Centers the Infographic.
+ */
+ get_header(); ?>
+<div class="clear"></div>
+<div class="page_content">
+    <div class="heading_container">
+        <?php compass_breadcrumbs(); ?>
+    </div>
+    <div class="alpha">
+        <div class="post single">
+            <div class="wp-block-image">
+                <figure class="aligncenter">
+                    <img src="https://ohldsmusic.org/wp-content/uploads/Infographic-1-791x1024.jpg" alt="" class="wp-image-386" srcset="https://ohldsmusic.org/wp-content/uploads/Infographic-1-791x1024.jpg 791w, https://ohldsmusic.org/wp-content/uploads/Infographic-1-232x300.jpg 232w, https://ohldsmusic.org/wp-content/uploads/Infographic-1-768x994.jpg 768w" sizes="(max-width: 791px) 100vw, 791px">
+                </figure>
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
+
+
